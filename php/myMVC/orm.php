04323951caa50917ba60e4c55169212ead4065c2
YvanGuo94 <838540487@qq.com>
2018-07-18 17:36:53 +0800

    qrcodephp and lluminate\Database, lluminate\Events

---
+++ php/myMVC/orm.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: onehome_gyf
+ * Date: 18/7/18
+ * Time: 下午5:17
+ */
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+require 'vendor/autoload.php';
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'database',
+    'username'  => 'root',
+    'password'  => 'password',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+// Set the event dispatcher used by Eloquent models... (optional)
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
---
+++ php/qc1.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: onehome_gyf
+ * Date: 18/7/18
+ * Time: 下午4:23
+ */
